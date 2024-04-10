9bb755919f2be8b019fd81fe65df6addd2681acc
olekszczepanowski <olekszczepanowski04@wp.pl>
2024-04-10 12:50:39 +0200

    added: all functionalities

---
+++ src/BooksDisplayer/BooksDisplayer.jsx
@@ -0,0 +1,166 @@
+import "./booksdisplayer.css";
+import React, { useState, useEffect } from 'react';
+
+function BooksDisplayer() {
+   const [records, setRecords] = useState([]);
+   const [expandedBook, setExpandedBook] = useState(null);
+   const [filteredBooks, setFilteredBooks] = useState([]);
+   const [favorites, setFavorites] = useState([]);
+   const [category, setCategory] = useState("All");
+   const [name, setName] = useState("");
+   const [isLoading, setIsLoading] = useState(true);
+
+   useEffect(() => {
+      setIsLoading(true);
+      fetch("https://gutendex.com/books")
+         .then(response => response.json())
+         .then(data => {
+            setRecords(data.results);
+            setIsLoading(false);
+         })
+         .catch(error => {
+            console.error(error);
+            setIsLoading(false);
+         });
+   }, [])
+
+   useEffect(() => {
+      const filteredBooks = filterByCategories(records, category);
+      setFilteredBooks(filteredBooks);
+      setExpandedBook(null);
+   }, [category, records, favorites]);
+
+   useEffect(() => {
+      const filteredBooks = filterByName(records, name);
+      setFilteredBooks(filteredBooks);
+      setExpandedBook(null);
+   }, [name, records]);
+   
+   function filterByCategories(records, category){
+      if (category === "All") {
+         return records;
+     }
+
+     if (category === "Favorite"){
+      return favorites;
+     }
+ 
+     const regex = new RegExp(category);
+ 
+     const filteredBooks = records.filter(book => {
+         const subjects = book.subjects || [];
+         return subjects.some(subject => regex.test(subject));
+     });
+ 
+     return filteredBooks;
+   }
+
+   function filterByName(records, name){
+      const nameToLowerCase = name.toLowerCase();
+      const filteredBooks = records.filter(book =>{
+         const title = book.title.toLowerCase();
+         return title.includes(nameToLowerCase);
+      });
+
+      return filteredBooks;
+   }
+
+   function handleInputChange(event){
+      setName(event.target.value);
+   }
+
+
+   function displayAuthors(authors){
+      let output = ``;
+      
+      if (authors.length > 1) {
+          const firstAuthor = authors[0].name;
+          let [lastNameOfFirstAuthor, firstNameOfFirstAuthor] = firstAuthor.split(", ")
+          
+          const secondAuthor = authors[1].name;
+          let [lastNameOfSecondAuthor, firstNameOfSecondAuthor] = secondAuthor.split(", ");
+          
+          if(firstNameOfFirstAuthor === undefined || firstNameOfSecondAuthor === undefined){
+              firstNameOfFirstAuthor = "";
+              firstNameOfSecondAuthor = ""; 
+          }
+          
+          output = `Authors: ${firstNameOfFirstAuthor} ${lastNameOfFirstAuthor}, ${firstNameOfSecondAuthor} ${lastNameOfSecondAuthor}`;
+      } else {
+          const author = authors[0].name;
+          let [lastName, firstName] = author.split(", ");
+          output = `Author: ${firstName} ${lastName}`;
+      }
+      return output;
+   }
+   
+   function toggleExpand(bookIndex) {
+      setExpandedBook(expandedBook === bookIndex ? null : bookIndex);
+   }
+
+   function toggleFavourite(bookIndex) {
+      const book = filteredBooks[bookIndex];
+      if (favorites.includes(book)) {
+         setFavorites(favorites.filter(id => id !== book));
+     } else {
+         setFavorites([...favorites, book]);
+     }
+  }
+
+   return (
+      <>
+      <div id="navbar">
+        <div id="categories"> 
+            <button className="category-button" onClick={() => setCategory("All")}>All</button>
+            <button className="category-button" onClick={() => setCategory("Fiction")}>Fiction</button>
+            <button className="category-button" onClick={() => setCategory("Drama")}>Drama</button>
+            <button className="category-button" onClick={() => setCategory("Horror")}>Horror</button>
+            <button className="category-button" onClick={() => setCategory("Favorite")}>Favourite</button>
+        </div>
+        
+        <div id="search-container">
+            <input id ="search-input" value= {name} onChange={handleInputChange}type="text" placeholder="Type in your book title!"></input>
+        </div>
+    </div>
+    {isLoading && <div id="loading-div">Loading...</div>}
+      
+      <div className="books-displayer">
+         <ul id="books-list">
+            
+            {filteredBooks.map((book, index) => (
+               <li className={`books-element ${favorites.includes(book) ? 'favorite' : ''}`} key={index}>
+                  <div className="upper-side-li">
+                     <div className="display-book-div">
+                        <div>
+                           "{book.title.includes("A Modest Proposal") ? 
+                              `A Modest Proposal` : book.title}"
+                        </div> 
+                        <div>
+                           {displayAuthors(book.authors)}
+                        </div>
+                     </div>
+                     <div className="right-side-li">
+                        <button className="expand-button" onClick={() => toggleExpand(index)}>
+                           <span className="material-symbols-outlined">menu_book</span>
+                           </button>
+                        <button className="favourite-button" onClick={() => toggleFavourite(index)}>
+                           <span className="material-symbols-outlined">favorite</span>
+                           </button>
+                     </div>
+                  </div>
+                  <div id="content-div" key={`content-${index}`}>
+               {expandedBook === index && (
+                  <iframe className="book-content" 
+                     src={book.formats["text/html"]} ></iframe>
+               )}
+            </div>
+               </li>
+            ))}
+         
+         </ul>
+      </div>
+      </>
+   );
+}
+
+export default BooksDisplayer;
---
+++ src/Header/Header.jsx
@@ -0,0 +1,8 @@
+import "./header.css"
+
+function Header(){
+    return(
+    <div><h1>Online library</h1></div>)
+}
+
+export default Header
